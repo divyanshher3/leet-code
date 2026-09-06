31a9797d4689306cfc162e8cb30eda884c54e2bc
divyansh shukla <divyanshshukla32@gmail.com>
2026-09-06 14:17:00 +0530

    Time: 0 ms (100%), Space: 10.8 MB (7.64%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    private:
+    bool valid(char ch){
+        if((ch>='a' && ch<='z')||(ch>='A' && ch <= 'Z')||(ch>='0' && ch<='9')){
+            return 1;
+        }
+        else{
+            return 0 ;
+        }
+    }
+    char Tolowercase(char ch){
+        if((ch >= 'a' && ch <= 'z' )||(ch>='0' && ch<='9')){
+            return ch;
+        }
+        else{
+            char temp = ch - 'A' + 'a';
+            return temp;
+        }
+    }
+
+    bool checkpalindrom(string a){
+        int s  = 0;
+        int e = a.length()-1;
+
+        while(s<=e){
+            if(a[s] != a[e] ){
+                return 0;
+            }
+            else{
+                s++;
+                e--;
+            }
+        }
+        return 1;
+    }
+public:
+
+    bool isPalindrome(string s) {
+        string temp = "" ; 
+
+        for(int  i =0 ; i < s.length() ; i++){
+            if(valid(s[i])){
+            temp.push_back(s[i]);
+            }
+        }
+
+        for(int i = 0 ; i<temp.length() ; i++){
+            temp[i] = Tolowercase(temp[i]);
+        }
+         
+        return checkpalindrom(temp);
+        
+    }
+};
